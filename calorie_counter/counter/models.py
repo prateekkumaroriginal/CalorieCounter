998379331982945edbf69abbedf4cb4fa34dba52
Prateek <prateek.kumar.original@gmail.com>
2023-11-25 16:14:17 +0530

    Add food

--- calorie_counter/counter/models.py
+++ calorie_counter/counter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Food(models.Model):
@@ -12,3 +13,9 @@ class Food(models.Model):
     def __str__(self):
         return self.name
     
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} --> {self.food_consumed}"
--- calorie_counter/counter/views.py
+++ calorie_counter/counter/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Consume
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        food_consumed = request.POST.get('food_consumed')
+        food = Food.objects.get(name=food_consumed)
+        user = request.user
+        Consume.objects.create(user=user, food_consumed=food)
+
+    consumed_foods = Consume.objects.filter(user=request.user)
     foods = Food.objects.all()
-    return render(request, 'counter/index.html', {'foods': foods})
+    return render(request, 'counter/index.html', {'foods': foods, 'consumed_foods': consumed_foods})
